3c1348342ef14a9ca8d2314632edaf782232b270
Tania Manco <belalov.tania16@gmail.com>
2019-11-26 03:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ const returnLastTwoDrivers = function(drivers){
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 const createFareMultiplier = function(num){
-  
+  return function(fareFee){
+    return num * fareFee
+  }
 }
