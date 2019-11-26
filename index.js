95a3c6ec867c35e547ac4e1af7ab20f8604f2073
Tania Manco <belalov.tania16@gmail.com>
2019-11-26 03:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ const returnLastTwoDrivers = function(drivers){
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const 
+const createFareMultiplier = function(num){
+  
+}
